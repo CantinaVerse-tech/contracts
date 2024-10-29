207b89538e02113a430dd05b23cd23bcf5a2772b
Riiz0 <shawnrizo@hotmail.com>
2024-10-29 19:52:41 -0400

    Feat: Create Vesting and UniswapRouter Contracts

---
+++ src/tokencreation/FactoryTokenContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FactoryTokenContract is Ownable {
+    constructor(address initialOwner) Ownable(initialOwner) { }
+}
---
+++ src/tokencreation/VestingContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VestingContract is Ownable {
+    using SafeERC20 for IERC20;
+
+    constructor(address initialOwner) Ownable(initialOwner) { }
+}
