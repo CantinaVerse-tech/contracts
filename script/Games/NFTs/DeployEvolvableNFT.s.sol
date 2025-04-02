d2eea33723a73fca8a94269a54b2fe3b8bb9cf7b
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-02 13:35:50 -0400

    Feat: Create Deployment Scripts for NFTRoulette & NFTStakingEvolution Games

---
+++ script/Games/NFTs/DeployEvolvableNFT.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../../HelperConfig.s.sol";
+import { EvolvableNFT } from "../../../src/games/NFTStakingEvolution/EvolvableNFT.sol";
+
+contract DeployEvolvableNFT is Script {
+    function run() external returns (EvolvableNFT) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        if (block.chainid == 31_337) {
+            config = helperConfig.getAnvilConfig();
+        } else if (block.chainid == 11_155_111) {
+            config = helperConfig.getSepoliaConfig();
+        } else if (block.chainid == 8453) {
+            config = helperConfig.getBaseMainnetConfig();
+        } else if (block.chainid == 84_532) {
+            config = helperConfig.getBaseSepoliaConfig();
+        } else if (block.chainid == 10) {
+            config = helperConfig.getOpMainnetConfig();
+        } else if (block.chainid == 11_155_420) {
+            config = helperConfig.getOPSepoliaConfig();
+        } else if (block.chainid == 34_443) {
+            config = helperConfig.getModeMainnetConfig();
+        } else {
+            revert("Unsupported network");
+        }
+
+        vm.startBroadcast();
+        EvolvableNFT game = new EvolvableNFT("https://ipfs.io/ipfs/");
+        vm.stopBroadcast();
+
+        console2.log("EvolvableNFT deployed at:", address(game));
+        console2.log("Initial owner:", config.initialOwner);
+        console2.log("Service fee:", config.serviceFee);
+
+        return game;
+    }
+}
---
+++ script/Games/NFTs/DeployPrizeNFT.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../../HelperConfig.s.sol";
+import { PrizeNFT } from "../../../src/games/NFTRoulette/PrizeNFT.sol";
+
+contract DeployPrizeNFT is Script {
+    function run() external returns (PrizeNFT) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        if (block.chainid == 31_337) {
+            config = helperConfig.getAnvilConfig();
+        } else if (block.chainid == 11_155_111) {
+            config = helperConfig.getSepoliaConfig();
+        } else if (block.chainid == 8453) {
+            config = helperConfig.getBaseMainnetConfig();
+        } else if (block.chainid == 84_532) {
+            config = helperConfig.getBaseSepoliaConfig();
+        } else if (block.chainid == 10) {
+            config = helperConfig.getOpMainnetConfig();
+        } else if (block.chainid == 11_155_420) {
+            config = helperConfig.getOPSepoliaConfig();
+        } else if (block.chainid == 34_443) {
+            config = helperConfig.getModeMainnetConfig();
+        } else {
+            revert("Unsupported network");
+        }
+
+        vm.startBroadcast();
+        PrizeNFT game = new PrizeNFT("TestPrizeNFT", "TPN", "https://ipfs.io/ipfs/");
+        vm.stopBroadcast();
+
+        console2.log("PrizeNFT deployed at:", address(game));
+        console2.log("Initial owner:", config.initialOwner);
+        console2.log("Service fee:", config.serviceFee);
+
+        return game;
+    }
+}
