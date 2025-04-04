b2f7ae62163ffa1a8728b6ce816706d74b323dbc
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-04 07:09:26 -0400

    Feat:

--- src/predictionmarket/PredictionMarketManager.sol
+++ src/predictionmarket/PredictionMarketManager.sol
@@ -11,6 +11,9 @@ import { PMLibrary } from "./lib/PMLibrary.sol";
  * @dev The contract allows whitelisted addresses to create prediction markets.
  */
 contract PredictionMarketManager is Ownable {
+    // Custom errors
+    error MarketFactory__CallerNotWhitelisted();
+
     // Whitelist state
     mapping(address => bool) public whitelistedAddresses;
 
@@ -18,4 +21,15 @@ contract PredictionMarketManager is Ownable {
      * @notice Constructor to initialize the factory contract
      */
     constructor() { }
+
+    /**
+     * @notice Modifier to restrict access to whitelisted addresses.
+     * @dev Reverts if the caller is not in the whitelist.
+     */
+    modifier onlyWhitelisted() {
+        if (!whitelistedAddresses[msg.sender]) {
+            revert MarketFactory__CallerNotWhitelisted();
+        }
+        _;
+    }
 }
