545e953929f24067db793c88604d9ff992375544
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-04 12:46:10 -0400

    Feat: Create PredictionMarket Contract and Add Imports
    Signed-off-by: Shawn Rizo <shawnrizo@hotmail.com>

--- src/predictionmarket/PredictionMarket.sol
+++ src/predictionmarket/PredictionMarket.sol
@@ -1,2 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
+
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { ExpandedERC20, ExpandedIERC20 } from "@uma/core/contracts/common/implementation/ExpandedERC20.sol";
+import {
+    OracleInterfaces,
+    OptimisticOracleConstraints
+} from "@uma/core/contracts/data-verification-mechanism/implementation/Constants.sol";
+import { AddressWhitelist } from "@uma/core/contracts/common/implementation/AddressWhitelist.sol";
+import { FinderInterface } from "@uma/core/contracts/data-verification-mechanism/interfaces/FinderInterface.sol";
+import { ClaimData } from "@uma/core/contracts/optimistic-oracle-v3/implementation/ClaimData.sol";
+import { OptimisticOracleV3Interface } from
+    "@uma/core/contracts/optimistic-oracle-v3/interfaces/OptimisticOracleV3Interface.sol";
+import { OptimisticOracleV3CallbackRecipientInterface } from
+    "@uma/core/contracts/optimistic-oracle-v3/interfaces/OptimisticOracleV3CallbackRecipientInterface.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IAMMContract } from "./interfaces/IAMMContract.sol";
+import { PMLibrary } from "./lib/PMLibrary.sol";
+import { PredictionMarketManager } from "./PredictionMarketManager.sol";
+
+/**
+ * @title PredictionMarket
+ * @author CantinaVerse
+ * @notice This contract allows users to create and participate in prediction markets using outcome tokens.
+ * @dev The contract integrates with Uniswap V3 for liquidity provision and UMA's Optimistic Oracle V3 for dispute
+ * resolution.
+ */
+contract PredictionMarket is OptimisticOracleV3CallbackRecipientInterface, Ownable, PredictionMarketManager { }
