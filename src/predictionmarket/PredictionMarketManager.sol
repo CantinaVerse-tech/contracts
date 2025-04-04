b63f157625a7a12147814502336d14fe7653b7bb
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-04 07:01:24 -0400

    Feat: PredictionMarketManager Contract Natspec

--- src/predictionmarket/PredictionMarketManager.sol
+++ src/predictionmarket/PredictionMarketManager.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { PMLibrary } from "./lib/PMLibrary.sol";
+
+/**
+ * @title PredictionMarketManager
+ * @author CantinaVerse
+ * @notice This contract is responsible for managing prediction markets.
+ * @dev The contract allows whitelisted addresses to create prediction markets.
+ */
+contract PredictionMarketManager is Ownable { }
