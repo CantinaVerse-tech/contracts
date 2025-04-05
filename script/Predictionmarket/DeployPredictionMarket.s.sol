34349e382f6491b09df0c0f5639dab6a9e87f73d
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-05 14:10:54 -0400

    Feat: Configure PredictionMarket Deployment Script
    Signed-off-by: Shawn Rizo <shawnrizo@hotmail.com>

--- script/Predictionmarket/DeployPredictionMarket.s.sol
+++ script/Predictionmarket/DeployPredictionMarket.s.sol
@@ -1,6 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import { Script } from "forge-std/Script.sol";
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+import { PredictionMarket } from "../../src/predictionmarket/PredictionMarket.sol";
 
-contract DeployPredictionMarket is Script { }
+contract DeployPredictionMarket is Script {
+    function run() external returns (PredictionMarket) {
+        HelperConfig helpConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        config = helpConfig.getBaseSepoliaConfig();
+
+        vm.startBroadcast();
+        PredictionMarket market = new PredictionMarket(
+            config.finder, config.currency, config.optimisticOracleV3, 0xD12355D121eDee77DbC4D1Abdf01A965409170e4
+        );
+        console2.log("PredictionMarket deployed to: ", address(market));
+        vm.stopBroadcast();
+        return market;
+    }
+}
