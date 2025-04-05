bed72e5344253ab6806f8bf4b85bed3c75bcdaa7
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-05 14:14:06 -0400

    Feat: Congifure PredictionMarketManager Deployment Script

--- script/Predictionmarket/DeployPredictionMarketManager.s.sol
+++ script/Predictionmarket/DeployPredictionMarketManager.s.sol
@@ -1,6 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import { Script } from "forge-std/Script.sol";
+import { Script, console2 } from "forge-std/Script.sol";
+import { PredictionMarketManager } from "../../src/predictionmarket/PredictionMarketManager.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
 
-contract DeployPreidictionMarketManager is Script { }
+contract DeployPredictionMarketManager is Script {
+    function run() external returns (PredictionMarketManager) {
+        HelperConfig helpConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        config = helpConfig.getBaseSepoliaConfig();
+
+        vm.startBroadcast();
+        PredictionMarketManager market = new PredictionMarketManager();
+        console2.log("PredictionMarketManager deployed to: ", address(market));
+        vm.stopBroadcast();
+        return market;
+    }
+}
