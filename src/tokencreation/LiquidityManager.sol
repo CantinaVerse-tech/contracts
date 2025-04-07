e80d43869289a2bd706bb9b3b3787a40c49a530e
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-07 13:17:58 -0400

    Feat: Add Public Immutable Variables and Constructor

--- src/tokencreation/LiquidityManager.sol
+++ src/tokencreation/LiquidityManager.sol
@@ -6,4 +6,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import { IUniswapV2Router02 } from "./interfaces/IUniswapV2Router02.sol";
 import { IUniswapV2Factory } from "./interfaces/IUniswapV2Factory.sol";
 
-contract LiquidityManager is Ownable { }
+contract LiquidityManager is Ownable {
+    IUniswapV2Router02 public immutable uniswapRouter;
+    address public immutable usdtAddress;
+
+    constructor(address _uniswapRouter, address _usdtAddress) {
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        usdtAddress = _usdtAddress;
+    }
+}
