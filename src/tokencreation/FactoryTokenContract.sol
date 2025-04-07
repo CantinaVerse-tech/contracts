77eec01b3a0027158db91c87985125e21e8ec7d8
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-07 13:34:48 -0400

    Feat: Create FactoryToken Contract

--- src/tokencreation/FactoryTokenContract.sol
+++ src/tokencreation/FactoryTokenContract.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.16;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { TokenContract } from "./TokenContract.sol";
+import { ILiquidityManager } from "./interfaces/ILiquidityManager.sol";
 
 contract FactoryTokenContract is Ownable {
     constructor() { }
