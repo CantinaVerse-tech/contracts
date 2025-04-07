ee7847d9497ff551749c94e04944a86da2c8c5eb
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-07 13:13:15 -0400

    Feat: Create LiquidityManager Contract

---
+++ src/tokencreation/LiquidityManager.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import { IUniswapV2Router02 } from "./interfaces/IUniswapV2Router02.sol";
+import { IUniswapV2Factory } from "./interfaces/IUniswapV2Factory.sol";
+
+contract LiquidityManager is Ownable { }
