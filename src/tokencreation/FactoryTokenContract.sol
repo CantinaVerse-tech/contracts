56d2431d85a5765cd8b44faba7548d04fed4e88e
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-07 14:11:34 -0400

    Feat: Add Func deployTokenAndAddLiquidity

--- src/tokencreation/FactoryTokenContract.sol
+++ src/tokencreation/FactoryTokenContract.sol
@@ -85,4 +85,53 @@ contract FactoryTokenContract is Ownable {
 
         return tokenAddr;
     }
+
+    /**
+     * @notice This function creates a token and adds liquidity
+     * @param tokenName The name of the token
+     * @param tokenSymbol The symbol of the token
+     * @param _initialSupply The initial supply
+     * @param _maxSupply The max supply
+     * @param _canMint Can mint
+     * @param _canBurn Cant Burn
+     * @param _supplyCapEnabled The supply cap enabled
+     * @param usdtAmount The USDT amount
+     */
+    function deployTokenAndAddLiquidity(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 _initialSupply,
+        uint256 _maxSupply,
+        bool _canMint,
+        bool _canBurn,
+        bool _supplyCapEnabled,
+        uint256 usdtAmount
+    )
+        external
+        returns (address)
+    {
+        // Deploy token
+        TokenContract token = new TokenContract(
+            msg.sender, tokenName, tokenSymbol, _initialSupply, _maxSupply, _canMint, _canBurn, _supplyCapEnabled
+        );
+
+        address tokenAddr = address(token);
+        deployedTokens.push(tokenAddr);
+        tokensByCreator[msg.sender].push(tokenAddr);
+
+        emit TokenCreated(tokenAddr, msg.sender, tokenName, tokenSymbol, _initialSupply);
+
+        // Ensure user has approved both tokens to this factory
+        require(IERC20(tokenAddr).transferFrom(msg.sender, address(this), _initialSupply), "Token transfer failed");
+        require(IERC20(usdtAddress).transferFrom(msg.sender, address(this), usdtAmount), "USDT transfer failed");
+
+        // Approve LiquidityManager
+        IERC20(tokenAddr).approve(liquidityManager, _initialSupply);
+        IERC20(usdtAddress).approve(liquidityManager, usdtAmount);
+
+        // Call LiquidityManager
+        ILiquidityManager(liquidityManager).addLiquidityToUniswap(tokenAddr, _initialSupply, usdtAmount);
+
+        return tokenAddr;
+    }
 }
