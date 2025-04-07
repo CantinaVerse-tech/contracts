0c12e862032e193b454ef2dc1709ff637cc8e359
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-07 14:00:16 -0400

    Feat: Add Func createToken and Event TokenCreated w/ Natspec

--- src/tokencreation/FactoryTokenContract.sol
+++ src/tokencreation/FactoryTokenContract.sol
@@ -19,6 +19,12 @@ contract FactoryTokenContract is Ownable {
     // @notice Map of tokens by creator
     mapping(address => address[]) public tokensByCreator;
 
+    // Events
+    // @notice Emitted when a token is created
+    event TokenCreated(
+        address indexed tokenAddress, address indexed creator, string name, string symbol, uint256 initialSupply
+    );
+
     /**
      * @notice This constructor takes in the liquidity manager and USDT address
      * @param _liquidityManager Is the liquidity manager
@@ -43,5 +49,40 @@ contract FactoryTokenContract is Ownable {
      */
     function setUSDTAddress(address _usdtAddress) external onlyOwner {
         usdtAddress = _usdtAddress;
+    }
+
+    /**
+     * @notice This function creates a token
+     * @param tokenName The name of the token
+     * @param tokenSymbol The symbol of the token
+     * @param _initialSupply The initial supply
+     * @param _maxSupply The max supply
+     * @param _canMint Can mint
+     * @param _canBurn Can Burn
+     * @param _supplyCapEnabled Supply cap enabled
+     */
+    function createToken(
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 _initialSupply,
+        uint256 _maxSupply,
+        bool _canMint,
+        bool _canBurn,
+        bool _supplyCapEnabled
+    )
+        external
+        returns (address)
+    {
+        TokenContract token = new TokenContract(
+            msg.sender, tokenName, tokenSymbol, _initialSupply, _maxSupply, _canMint, _canBurn, _supplyCapEnabled
+        );
+
+        address tokenAddr = address(token);
+        deployedTokens.push(tokenAddr);
+        tokensByCreator[msg.sender].push(tokenAddr);
+
+        emit TokenCreated(tokenAddr, msg.sender, tokenName, tokenSymbol, _initialSupply);
+
+        return tokenAddr;
     }
 }
