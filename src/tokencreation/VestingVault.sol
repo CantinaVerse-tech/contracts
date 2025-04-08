fb443f0eaa1ae38082ff6d5c1ad561469a5e2972
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-08 07:29:57 -0400

    Feat: Immuntiable Varible, Struct, and Mapping Created

--- src/tokencreation/VestingVault.sol
+++ src/tokencreation/VestingVault.sol
@@ -4,4 +4,15 @@ pragma solidity 0.8.16;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VestingVault is Ownable { }
+contract VestingVault is Ownable {
+    struct VestingSchedule {
+        uint256 totalAmount; // Total tokens locked for the user
+        uint256 claimedAmount; // How much they’ve already claimed
+        uint256 startTime; // When vesting begins
+        uint256 cliffTime; // When first claim is allowed
+        uint256 duration; // Total vesting duration (e.g. 10 months)
+    }
+
+    IERC20 public immutable token;
+    mapping(address => VestingSchedule) public schedules;
+}
