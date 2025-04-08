52f27f670df8f1655c36fed24d65a6283423f337
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-08 07:38:18 -0400

    Feat: Added Natspec and Constructor

--- src/tokencreation/VestingVault.sol
+++ src/tokencreation/VestingVault.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract VestingVault is Ownable {
+    // @notice The vesting schedule for a user
     struct VestingSchedule {
         uint256 totalAmount; // Total tokens locked for the user
         uint256 claimedAmount; // How much they’ve already claimed
@@ -13,6 +14,17 @@ contract VestingVault is Ownable {
         uint256 duration; // Total vesting duration (e.g. 10 months)
     }
 
+    // @notice The token being held in the vault
     IERC20 public immutable token;
+
+    // @notice The vesting schedules for each user
     mapping(address => VestingSchedule) public schedules;
+
+    /**
+     * @notice Constructor to set the token address
+     * @param _token The token being held in the vault
+     */
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
 }
