e2a26040b198ffdedd2925129bd4825d92546e76
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-09 10:27:43 -0400

    Setup DeployFactoryTokenContract Script

---
+++ script/TokenCreation/DeployFactoryTokenContract.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+
+contract DeployAMMContract is Script {
+    function run() external { }
+}
