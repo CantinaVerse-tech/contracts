9a3bcc9163d302c9556d58d545f66c2f3b66a6c4
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-09 10:35:58 -0400

    Setup DeployPresaleManager Script

--- script/TokenCreation/DeployFactoryTokenContract.s.sol
+++ script/TokenCreation/DeployFactoryTokenContract.s.sol
@@ -4,6 +4,6 @@ pragma solidity 0.8.16;
 import { Script, console2 } from "forge-std/Script.sol";
 import { HelperConfig } from "../HelperConfig.s.sol";
 
-contract DeployAMMContract is Script {
+contract DeployFactoryTokenContract is Script {
     function run() external { }
 }
---
+++ script/TokenCreation/DeployPresaleManager.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+
+contract DeployPresaleManager is Script {
+    function run() external { }
+}
