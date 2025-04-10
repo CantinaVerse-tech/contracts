1e4aafeb52696b4dc5f697fd7eed1c8e71dd8d2a
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-10 01:42:49 -0400

    Feat: Setup CantinaVerseGovernor Import Libraries, and Natspec

--- src/governance/CantinaVerseGovernor.sol
+++ src/governance/CantinaVerseGovernor.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-contract CantinaVerseGovernor { }
+import { Governor } from "@openzeppelin/contracts/governance/Governor.sol";
+import { GovernorCountingSimple } from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import { GovernorSettings } from "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import { GovernorTimelockControl } from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import { GovernorVotes } from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import { GovernorVotesQuorumFraction } from
+    "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import { IVotes } from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+/**
+ * @title CantinaVerseGovernor
+ * @author CantinaVerse-Tech
+ * @notice Governance contract for CantinaVerse ecosystem
+ * @dev Implements OpenZeppelin Governor with multiple extensions
+ */
+contract CantinaVerseGovernor is
+    Governor,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorVotes,
+    GovernorVotesQuorumFraction,
+    GovernorTimelockControl
+{ }
