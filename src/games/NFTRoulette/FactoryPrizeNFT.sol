3c459fe45205e562bc152551815cf1e89cfb4b25
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-15 14:19:40 -0400

    Feat: Create FactoryPrizeNFT Contract For NFTRoulette Game

---
+++ src/games/NFTRoulette/FactoryPrizeNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { PrizeNFT } from "./PrizeNFT.sol";
+
+/**
+ * @title FactoryPrizeNFT
+ * @author CantinaVerse-Tech
+ * @notice Factory contract to deploy and manage multiple PrizeNFT collections
+ * @dev This contract allows for the creation and management of multiple PrizeNFT collections.
+ */
+contract FactoryPrizeNFT { }
