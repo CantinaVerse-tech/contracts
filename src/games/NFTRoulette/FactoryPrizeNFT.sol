37cfb2fda48ad75d4067cb38f4a0ed44daaa00af
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-15 14:30:49 -0400

    Feat: Add Ownable Inheritance, Add Constructor and Add Func createPrizeNFT

--- src/games/NFTRoulette/FactoryPrizeNFT.sol
+++ src/games/NFTRoulette/FactoryPrizeNFT.sol
@@ -10,7 +10,36 @@ import { PrizeNFT } from "./PrizeNFT.sol";
  * @notice Factory contract to deploy and manage multiple PrizeNFT collections
  * @dev This contract allows for the creation and management of multiple PrizeNFT collections.
  */
-contract FactoryPrizeNFT {
+contract FactoryPrizeNFT is Ownable {
     // Array to keep track of all deployed PrizeNFT contracts
     address[] public allPrizeNFTs;
+
+    constructor() { }
+
+    /**
+     * @dev Deploys a new PrizeNFT contract
+     * @param name Name of the NFT collection
+     * @param symbol Symbol of the NFT collection
+     * @param baseURI Base URI for token metadata
+     * @return prizeNFT Address of the newly deployed PrizeNFT contract
+     */
+    function createPrizeNFT(
+        string memory name,
+        string memory symbol,
+        string memory baseURI
+    )
+        external
+        onlyOwner
+        returns (address prizeNFT)
+    {
+        // Deploy a new PrizeNFT contract
+        PrizeNFT newPrizeNFT = new PrizeNFT(name, symbol, baseURI);
+        // Transfer ownership of the new contract to the owner of the factory
+        newPrizeNFT.transferOwnership(owner());
+        // Add the new contract's address to the tracking array
+        allPrizeNFTs.push(address(newPrizeNFT));
+        // Emit an event for the new contract creation
+        emit PrizeNFTCreated(address(newPrizeNFT), name, symbol, baseURI);
+        return address(newPrizeNFT);
+    }
 }
