d32bb1be1c2383199fe4b74124a432416ef45dc8
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-22 00:07:35 -0400

    Edit: Openzeppelin Lib Imports In CoinFlip Contract

--- src/games/CoinFlip.sol
+++ src/games/CoinFlip.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * @title Coin Flip Game
