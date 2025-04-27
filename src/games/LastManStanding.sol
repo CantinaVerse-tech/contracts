3697c684287a2024c4bc577b80bc99ac3678731d
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-27 15:35:48 -0400

    Feat: Create LastManStanding Contract

---
+++ src/games/LastManStanding.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+/// @title Last Man Standing
+/// @author CantinaVerse-Tech
+/// @notice Anyone can create a Last Man Standing game. Last player before time runs out wins the pot.
+contract LastManStanding { }
