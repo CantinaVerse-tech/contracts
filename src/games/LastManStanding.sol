354f295c89de78a5b95299f8e69070bc4ed4a83b
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-27 15:41:33 -0400

    Feat: Add Struct Game

--- src/games/LastManStanding.sol
+++ src/games/LastManStanding.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
-/// @title Last Man Standing
+/// @title LastManStanding
 /// @author CantinaVerse-Tech
 /// @notice Anyone can create a Last Man Standing game. Last player before time runs out wins the pot.
-contract LastManStanding { }
+contract LastManStanding {
+    // @notice Game struct to hold individual game data
+    struct Game {
+        uint256 entryFee;
+        uint256 timeExtension; // in seconds
+        uint256 endTime;
+        address lastPlayer;
+        bool active;
+        uint256 pot;
+        address creator;
+    }
+}
