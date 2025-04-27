5c0b75b3ddbe7c0e75eb818a68fc18240b842983
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-27 15:48:12 -0400

    Feat: Add Events

--- src/games/LastManStanding.sol
+++ src/games/LastManStanding.sol
@@ -21,4 +21,13 @@ contract LastManStanding {
 
     // @notice Map of gameId to Game data
     mapping(uint256 => Game) public games;
+
+    // @notice Event emitted when a game is created
+    event GameCreated(uint256 indexed gameId, address indexed creator, uint256 entryFee, uint256 timeExtension);
+
+    // @notice Event emitted when a player joins a game
+    event PlayerJoined(uint256 indexed gameId, address indexed player, uint256 newEndTime);
+
+    // @notice Event emitted when a game ends
+    event PrizeClaimed(uint256 indexed gameId, address indexed winner, uint256 prizeAmount);
 }
