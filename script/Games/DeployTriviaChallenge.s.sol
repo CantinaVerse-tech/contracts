ef86b2015912f87941802f14992bdd970e3d9e6c
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-26 22:45:30 -0400

    Feat: Create TriviaChallenge Deployment Script

---
+++ script/Games/DeployTriviaChallenge.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+import { TriviaChallenge } from "../../src/games/TriviaChallenge.sol";
+
+contract DeployFactoryTokenContract is Script {
+    function run() external { }
+}
--- script/Governance/DeployCantinaToken.s.sol
+++ script/Governance/DeployCantinaToken.s.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.16;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { HelperConfig } from "../HelperConfig.s.sol";
-import { CantinaToken } from "../../src/governance//CantinaToken.sol";
+import { CantinaToken } from "../../src/governance/CantinaToken.sol";
 
 contract DeployFactoryTokenContract is Script {
     function run() external {
