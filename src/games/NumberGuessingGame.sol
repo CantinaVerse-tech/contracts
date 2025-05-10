f0ae28543fe6649f3637756c5db129a410f336ea
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-10 17:22:56 -0400

    Feat: Add Storage Variables

--- src/games/NumberGuessingGame.sol
+++ src/games/NumberGuessingGame.sol
@@ -10,4 +10,25 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  * @dev A simple number guessing game where players pay a fee to guess a number.
  *      If a player guesses correctly, they win the accumulated jackpot.
  */
-contract NumberGuessingGame is Ownable, ReentrancyGuard { }
+contract NumberGuessingGame is Ownable, ReentrancyGuard {
+    /// @notice The secret number to be guessed (0-255)
+    uint8 private secretNumber;
+
+    /// @notice The fee required to make a guess (in wei)
+    uint256 public guessFee;
+
+    /// @notice The current jackpot amount (in wei)
+    uint256 public jackpot;
+
+    /// @notice Indicates whether the game is currently active
+    bool public isActive;
+
+    /// @notice Maximum number of attempts allowed before game ends
+    uint256 public maxAttempts;
+
+    /// @notice Current number of attempts made
+    uint256 public attemptCount;
+
+    /// @notice Address of the winner
+    address public winner;
+}
