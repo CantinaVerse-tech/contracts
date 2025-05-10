2ae932c9fb4dcbb560417034d1fda906faf248b5
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-10 17:55:45 -0400

    Feat: Create Deployment Script For The NumberGuessingGame

---
+++ script/Games/DeployNumberGuessingGame.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+import { NumberGuessingGame } from "../../src/games/NumberGuessingGame.sol";
+
+contract DeployNFTRoulette is Script { }
