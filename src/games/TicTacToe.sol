c42e8e79ddacfac69baeb9cb09a0d5073c9b6450
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-11 14:12:57 -0400

    Feat: Create TicTacToe Game Contract

---
+++ src/games/TicTacToe.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+/**
+ * @title TicTacToe
+ * @author CantinaVerse-Tech
+ * @dev A two-player Tic-Tac-Toe game implemented as a smart contract.
+ */
+contract TicTacToe is ReentrancyGuard { }
