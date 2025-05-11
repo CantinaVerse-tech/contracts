468a4f68b0e72bae78810f6456cf7b504ab5417e
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-11 14:35:57 -0400

    Edit: Update TicTacToe Imports

--- src/games/TicTacToe.sol
+++ src/games/TicTacToe.sol
@@ -2,10 +2,11 @@
 pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title TicTacToe
  * @author CantinaVerse-Tech
  * @dev A two-player Tic-Tac-Toe game implemented as a smart contract.
  */
-contract TicTacToe is ReentrancyGuard { }
+contract TicTacToe is ReentrancyGuard, Ownable { }
