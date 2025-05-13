c051279a76e1ce185f17c28b3c61beecf63cab50
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-13 00:05:53 -0400

    Feat: Create Deployment Script for TicTacToe Game

---
+++ script/Games/DeployTicTacToe.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+import { TicTacToe } from "../../src/games/TicTacToe.sol";
+
+contract DeployTicTacToe is Script { }
