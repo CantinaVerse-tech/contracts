78f87e2a57a50ee4dc8136a58396b4d7b25f9f73
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-13 00:10:40 -0400

    Feat: Add run Function inside of TicTacToe Deployment Script

--- script/Games/DeployTicTacToe.s.sol
+++ script/Games/DeployTicTacToe.s.sol
@@ -5,4 +5,17 @@ import { Script, console2 } from "forge-std/Script.sol";
 import { HelperConfig } from "../HelperConfig.s.sol";
 import { TicTacToe } from "../../src/games/TicTacToe.sol";
 
-contract DeployTicTacToe is Script { }
+contract DeployTicTacToe is Script {
+    function run() external returns (TicTacToe) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        vm.startBroadcast();
+        TicTacToe ticTacToe = new TicTacToe();
+        vm.stopBroadcast();
+
+        console2.log("TicTacToe deployed at:", address(ticTacToe));
+
+        return ticTacToe;
+    }
+}
