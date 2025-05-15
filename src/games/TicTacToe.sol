fc8487153cbb2bae5c922c7db998fd9546c17740
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-15 14:27:39 -0400

    Edit: Add makeMove Func

--- src/games/TicTacToe.sol
+++ src/games/TicTacToe.sol
@@ -70,26 +70,37 @@ contract TicTacToe is ReentrancyGuard {
     }
 
     /**
-     * @notice Checks if the specified player has won.
-     * @param player The player to check.
-     * @return True if the player has won, false otherwise.
+     * @notice Makes a move in the specified game.
+     * @param gameId The identifier of the game.
+     * @param row The row index (0-2).
+     * @param col The column index (0-2).
      */
-    function checkWin(Player player) internal view returns (bool) {
-        for (uint8 i = 0; i < 3; i++) {
-            if (
-                (board[i][0] == player && board[i][1] == player && board[i][2] == player)
-                    || (board[0][i] == player && board[1][i] == player && board[2][i] == player)
-            ) {
-                return true;
-            }
+    function makeMove(uint256 gameId, uint8 row, uint8 col) external {
+        Game storage game = games[gameId];
+        require(game.gameState == GameState.InProgress, "Game is not in progress");
+        require(row < 3 && col < 3, "Invalid board position");
+        require(game.board[row][col] == Player.None, "Cell already occupied");
+
+        if (game.currentPlayer == Player.X) {
+            require(msg.sender == game.playerX, "Not your turn");
+            game.board[row][col] = Player.X;
+            game.currentPlayer = Player.O;
+        } else {
+            require(msg.sender == game.playerO, "Not your turn");
+            game.board[row][col] = Player.O;
+            game.currentPlayer = Player.X;
         }
-        if (
-            (board[0][0] == player && board[1][1] == player && board[2][2] == player)
-                || (board[0][2] == player && board[1][1] == player && board[2][0] == player)
-        ) {
-            return true;
+
+        emit MoveMade(gameId, msg.sender, row, col);
+
+        if (checkWin(game.board, game.board[row][col])) {
+            game.gameState = GameState.Finished;
+            game.winner = msg.sender;
+            emit GameWon(gameId, msg.sender);
+        } else if (isBoardFull(game.board)) {
+            game.gameState = GameState.Finished;
+            emit GameDraw(gameId);
         }
-        return false;
     }
 
     /**
