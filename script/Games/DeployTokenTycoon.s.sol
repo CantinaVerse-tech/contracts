811e10042f9d15957540b1df370f168cb6e11ff9
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-17 18:14:08 -0400

    Feat: Configure TokenTycoon Deployment Script

--- script/Games/DeployTokenTycoon.s.sol
+++ script/Games/DeployTokenTycoon.s.sol
@@ -5,4 +5,17 @@ import { Script, console2 } from "forge-std/Script.sol";
 import { HelperConfig } from "../HelperConfig.s.sol";
 import { TokenTycoon } from "../../src/games/TokenTycoon.sol";
 
-contract DeployTokenTycoon is Script { }
+contract DeployTokenTycoon is Script {
+    function run() external returns (TokenTycoon) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        vm.startBroadcast();
+        TokenTycoon tokenTycoon = new TokenTycoon();
+        vm.stopBroadcast();
+
+        console2.log("TokenTycoon deployed at:", address(tokenTycoon));
+
+        return tokenTycoon;
+    }
+}
