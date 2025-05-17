048af7737041f71a66b345e10f1c0f020910705f
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-17 18:06:23 -0400

    Feat: Create TokenTycoon Deployment Script

--- script/Games/DeployTokenTycoon.s.sol
+++ script/Games/DeployTokenTycoon.s.sol
@@ -1,1 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
 
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+import { TokenTycoon } from "../../src/games/TokenTycoon.sol";
+
+contract DeployTokenTycoon is Script { }
