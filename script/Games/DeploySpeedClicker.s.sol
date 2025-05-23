5fea15d0feda24ac2b663614c97ff43d5110b583
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-23 11:39:46 -0400

    Feat: Configure And Setup SpeedClickerChallenge Deployment Script

--- script/Games/DeploySpeedClicker.s.sol
+++ script/Games/DeploySpeedClicker.s.sol
@@ -5,4 +5,17 @@ import { Script, console2 } from "forge-std/Script.sol";
 import { HelperConfig } from "../HelperConfig.s.sol";
 import { SpeedClickerChallenge } from "../../../src/games/SpeedClickerChallenge.sol";
 
-contract DeploySpeedClickerChallenge is Script { }
+contract DeploySpeedClickerChallenge is Script {
+    function run() external returns (SpeedClickerChallenge) {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config;
+
+        vm.startBroadcast();
+        SpeedClickerChallenge speedClickerChallenge = new SpeedClickerChallenge();
+        vm.stopBroadcast();
+
+        console2.log("SpeedClickerChallenge deployed at:", address(speedClickerChallenge));
+
+        return speedClickerChallenge;
+    }
+}
