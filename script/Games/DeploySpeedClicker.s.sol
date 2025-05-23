397b0aeadb08fd18a4dc90e361a5f26b280da60d
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-23 11:36:47 -0400

    Feat: Create SpeedClickerChallenge Deployment Script

---
+++ script/Games/DeploySpeedClicker.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+import { Script, console2 } from "forge-std/Script.sol";
+import { HelperConfig } from "../HelperConfig.s.sol";
+import { SpeedClickerChallenge } from "../../../src/games/SpeedClickerChallenge.sol";
+
+contract DeploySpeedClickerChallenge is Script { }
