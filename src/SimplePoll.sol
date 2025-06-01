3ab699a4126547d42d535bc64920e7a6481d704d
Shawn Rizo <shawnrizo@hotmail.com>
2025-05-31 23:53:51 -0400

    Feat: Create & Configure SimplePoll Contract

---
+++ src/SimplePoll.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+contract SimplePoll {
+    bytes32 public question;
+    mapping(address => bool) public votes; // true = yes, false = no
+    mapping(address => bool) public hasVoted;
+    uint256 public yesCount;
+    uint256 public noCount;
+
+    constructor(bytes32 _question) {
+        question = _question;
+    }
+
+    function vote(bool _vote) external {
+        require(!hasVoted[msg.sender], "Already voted");
+
+        votes[msg.sender] = _vote;
+        hasVoted[msg.sender] = true;
+
+        if (_vote) {
+            yesCount++;
+        } else {
+            noCount++;
+        }
+    }
+
+    function getResults() external view returns (uint256 yes, uint256 no) {
+        return (yesCount, noCount);
+    }
+}
