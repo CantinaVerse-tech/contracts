e56a3436afd268566ddee9265735e87229deb52f
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-02 22:49:23 -0400

    Feat: Create & Configure MessageWall Contract

---
+++ src/MessageWall.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
+
+contract MessageWall {
+    mapping(address => mapping(address => bytes32)) public messages;
+
+    // Leave a message for someone
+    function leaveMessage(address recipient, bytes32 message) external {
+        messages[msg.sender][recipient] = message;
+    }
+
+    // Read message from someone
+    function readMessage(address sender) external view returns (bytes32) {
+        return messages[sender][msg.sender];
+    }
+}
