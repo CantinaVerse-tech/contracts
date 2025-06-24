f7f2d78fbaf64aafa2fc70d6311113e3b7103c3b
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-24 13:42:50 -0400

    Feat: Add Full Natspec to NumberGuessing Contract In Academic Learning Folder

--- src/academic-learning/NumberGuesser.sol
+++ src/academic-learning/NumberGuesser.sol
@@ -10,6 +10,11 @@ contract NumberGuesser {
         secretHash = keccak256(abi.encodePacked(_secretNumber));
     }
 
+    /**
+     * @notice Allows a player to guess the secret number.
+     * @param _guess The player's guess.
+     * @dev If the guess is correct, the game ends and the player becomes the winner.
+     */
     function guess(uint256 _guess) external {
         require(gameActive, "Game over");
 
