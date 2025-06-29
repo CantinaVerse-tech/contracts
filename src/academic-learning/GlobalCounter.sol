7f014b980bf0f07125c0bea9384a4aec0bbe126b
Shawn Rizo <shawnrizo@hotmail.com>
2025-06-29 14:05:50 -0400

    Feat: Add Full Natspec For GlobalCounter

--- src/academic-learning/GlobalCounter.sol
+++ src/academic-learning/GlobalCounter.sol
@@ -6,10 +6,18 @@ contract GlobalCounter {
     // @notice The number of times this contract has been incremented
     uint256 public count;
 
+    /**
+     * @notice Increments the count by 1
+     * @dev This function can be called by anyone
+     */
     function increment() external {
         count++;
     }
 
+    /**
+     * @notice Returns the current count
+     * @dev This function can be called by anyone
+     */
     function getCount() external view returns (uint256) {
         return count;
     }
