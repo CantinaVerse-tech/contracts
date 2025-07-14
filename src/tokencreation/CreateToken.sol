58183d76876f5b6ea12875cc137444346cdee09b
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-14 18:15:55 -0400

    Feat: Add Constructor Func

--- src/tokencreation/CreateToken.sol
+++ src/tokencreation/CreateToken.sol
@@ -17,4 +17,28 @@ contract SimpleToken is ERC20, Ownable {
     address public creator;
 
     event TokenCreated(address indexed token, address indexed creator, string name, string symbol, uint256 totalSupply);
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals_,
+        uint256 totalSupply,
+        string memory description,
+        string memory imageUrl,
+        address creator_
+    )
+        ERC20(name, symbol)
+        Ownable(creator_)
+    {
+        _decimals = decimals_;
+        maxSupply = totalSupply;
+        _description = description;
+        _imageUrl = imageUrl;
+        creator = creator_;
+
+        // Mint total supply to creator
+        _mint(creator_, totalSupply);
+
+        emit TokenCreated(address(this), creator_, name, symbol, totalSupply);
+    }
 }
