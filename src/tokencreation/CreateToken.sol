1b885b67ba2d30ee79dda9365b36617ccb08e495
Shawn Rizo <shawnrizo@hotmail.com>
2025-07-14 17:58:45 -0400

    Feat: Add pragma, imports and contract inheritance

---
+++ src/tokencreation/CreateToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title CreateToken
+ * @author CantinaVerse-Tech
+ * @notice
+ */
+contract SimpleToken is ERC20, Ownable { }
