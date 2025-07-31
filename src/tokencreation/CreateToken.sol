54e6273318b049e9402276099d8c72eafbf68633
cantinaverse <cantinaverse@gmail.com>
2025-07-31 10:41:03 -0400

    Feat: Add updateFeeRecipient Func Full Natspec Comments

--- src/tokencreation/CreateToken.sol
+++ src/tokencreation/CreateToken.sol
@@ -647,6 +647,12 @@ contract TokenFactory {
         emit CreationFeeUpdated(newFee);
     }
 
+    /**
+     * @notice Updates the address that receives creation fees
+     * @dev Can only be called by the current fee recipient
+     *      The new recipient also becomes the contract owner
+     * @param newRecipient The new fee recipient address (cannot be zero address)
+     */
     function updateFeeRecipient(address newRecipient) external onlyOwner {
         require(newRecipient != address(0), "Invalid recipient");
         feeRecipient = newRecipient;
