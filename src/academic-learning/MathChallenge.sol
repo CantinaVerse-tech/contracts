e207fcca8b449198efcb9df132d747cc519ab008
cantinaverse <cantinaverse@gmail.com>
2025-08-01 09:53:40 -0400

    Feat: Add Storage Vars

--- src/academic-learning/MathChallenge.sol
+++ src/academic-learning/MathChallenge.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MathChallenge {}
+contract MathChallenge {
+    string public problem;
+    uint256 public immutable correctAnswer;
+    string public hint;
+    uint256 public difficulty; // 1-5 scale
+    
+}
