900d2a052139fe97c41132579404e1937c6c8802
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-11 13:53:03 -0400

    Feat: Update MultipleChoiceQuiz.sol Add addQuestion Func

--- src/academic-learning/MultipleChoiceQuiz.sol
+++ src/academic-learning/MultipleChoiceQuiz.sol
@@ -19,11 +19,28 @@ contract MultipleChoiceQuiz {
   mapping(address => bool) public passed;
 
   event QuizSubmitted(address indexed student, uint256 score, bool passed);
+
   constructor(
         string memory _title,
         uint256 _passingScore
     ) {
         quizTitle = _title;
         passingScore = _passingScore;
+    }
+
+function addQuestion(
+        string memory _question,
+        string[] memory _options,
+        uint8 _correctAnswer,
+        string memory _explanation
+    ) external {
+        require(_correctAnswer < _options.length, "Invalid correct answer index");
+        
+        questions.push(Question({
+            question: _question,
+            options: _options,
+            correctAnswer: _correctAnswer,
+            explanation: _explanation
+        }));
     }
 }
