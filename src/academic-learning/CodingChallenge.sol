c1f22931853dd670946430afff90631a54cc6678
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-18 00:45:12 -0400

    Feat: Update CodingChallenge.sol Add Detailed Natspec Comments For challengeTitle Var

--- src/academic-learning/CodingChallenge.sol
+++ src/academic-learning/CodingChallenge.sol
@@ -8,7 +8,10 @@ pragma solidity ^0.8.19;
  * @notice This contract allows instructors to create coding challenges and students to submit solutions
  */
 contract CodingChallenge {
+    /// @notice The title of the coding challenge
+    /// @dev Publicly accessible string containing the challenge name
     string public challengeTitle;
+
     string public description;
     string public requirements;
     bytes32[] public testCaseHashes; // Hashes of expected outputs
