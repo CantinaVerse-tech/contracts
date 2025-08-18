2a335801f8cf846a0481427646fbde650e560669
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-18 00:44:43 -0400

    Feat: Update CodingChallenge.sol Add Detailed Title Natspec Comments

--- src/academic-learning/CodingChallenge.sol
+++ src/academic-learning/CodingChallenge.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title CodingChallenge
+ * @author CantinaVerse-Tech
+ * @dev A smart contract for managing coding challenges with automated test case validation
+ * @notice This contract allows instructors to create coding challenges and students to submit solutions
+ */
 contract CodingChallenge {
     string public challengeTitle;
     string public description;
