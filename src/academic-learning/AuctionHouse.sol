580e3be5c21bb634dbcd3698936bac8d8a86d7be
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-27 09:09:27 -0400

    Feat: Add Contract Title Natspec Comments

--- src/academic-learning/AuctionHouse.sol
+++ src/academic-learning/AuctionHouse.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title AuctionHouse
+ * @dev English auction system with bidding, withdrawal, and settlement
+ * Educational contract for learning auction mechanics
+ */
 contract AuctionHouse {}
