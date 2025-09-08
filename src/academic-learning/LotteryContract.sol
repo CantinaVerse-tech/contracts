7cc2d851253cfc3e1e45216fe8ba34356eb04887
CantinaVerse Intern <cantinaverse@gmail.com>
2025-09-08 13:01:11 -0400

    feat: Add Contract Title Natspec Comments

--- src/academic-learning/LotteryContract.sol
+++ src/academic-learning/LotteryContract.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title LotteryContract
+ * @dev Simple lottery system with ticket purchases and random winner selection
+ * Educational contract for learning randomness and lottery mechanics
+ */
 contract LotteryContract {}
