31c0a4dede4d373a54e3fa26118b666bfdf04964
cantinaverse <cantinaverse@gmail.com>
2025-09-20 12:02:29 -0700

    feat: Add State Varibales

--- src/academic-learning/TaskBounty.sol
+++ src/academic-learning/TaskBounty.sol
@@ -28,4 +28,8 @@ contract TaskBounty {
         uint256 submittedAt;
         bool isAccepted;
     }
+
+        // State variables
+    uint256 private nextTaskId;
+    uint256 private nextSubmissionId;
 }
