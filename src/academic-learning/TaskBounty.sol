31536497560ae2d8ec8840e6496a076198c8587e
cantinaverse <cantinaverse@gmail.com>
2025-09-21 09:21:53 -0700

    feat: Add userSubmissions Mapping

--- src/academic-learning/TaskBounty.sol
+++ src/academic-learning/TaskBounty.sol
@@ -37,4 +37,5 @@ contract TaskBounty {
     mapping(uint256 => Submission) public submissions;
     mapping(uint256 => uint256[]) public taskSubmissions; // taskId => submission IDs
      mapping(address => uint256[]) public userTasks; // user => task IDs created
+         mapping(address => uint256[]) public userSubmissions; // user => submission IDs
 }
