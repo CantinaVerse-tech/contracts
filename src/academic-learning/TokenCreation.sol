0ddcf90ee3c8df99a10f00c05515835618f435c6
cantinaverse <cantinaverse@gmail.com>
2025-10-03 07:33:39 -0700

    feat: Add Contract Title Natspec Comments

--- src/academic-learning/TokenCreation.sol
+++ src/academic-learning/TokenCreation.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title TokenCreation
+ * @dev Basic ERC20 token that can be deployed by the factory
+ */
 contract TokenCreation {}
