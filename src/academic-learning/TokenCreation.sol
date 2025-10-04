5ee972a35a9ec50291b0b9757b5afa88430f3117
cantinaverse <cantinaverse@gmail.com>
2025-10-04 12:13:22 -0700

    feat: forge fmt

--- src/academic-learning/TokenCreation.sol
+++ src/academic-learning/TokenCreation.sol
@@ -8,10 +8,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @title TokenCreation
  * @dev Basic ERC20 token that can be deployed by the factory
  */
- contract TokenCreation is ERC20, Ownable {
-
-// Storage Variables
-        uint8 private _decimals;
+contract TokenCreation is ERC20, Ownable {
+    // Storage Variables
+    uint8 private _decimals;
     uint256 public maxSupply;
     string private _description;
     string private _imageUrl;
