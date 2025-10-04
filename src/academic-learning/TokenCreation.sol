c79c06ca55f9263491a8db121456594b0c9b950a
cantinaverse <cantinaverse@gmail.com>
2025-10-04 12:12:42 -0700

    feat: Add Storage Variables & Section Natspec

--- src/academic-learning/TokenCreation.sol
+++ src/academic-learning/TokenCreation.sol
@@ -8,4 +8,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @title TokenCreation
  * @dev Basic ERC20 token that can be deployed by the factory
  */
-contract TokenCreation { }
+contract TokenCreation { 
+
+// Storage Variables
+        uint8 private _decimals;
+    uint256 public maxSupply;
+    string private _description;
+    string private _imageUrl;
+    address public creator;
+}
