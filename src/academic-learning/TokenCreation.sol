aeb87e115464c09ef2778eff1185c5ffd927c9ef
cantinaverse <cantinaverse@gmail.com>
2025-10-04 12:13:11 -0700

    feat: Add Contract Inheritance

--- src/academic-learning/TokenCreation.sol
+++ src/academic-learning/TokenCreation.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  * @title TokenCreation
  * @dev Basic ERC20 token that can be deployed by the factory
  */
-contract TokenCreation { 
+ contract TokenCreation is ERC20, Ownable {
 
 // Storage Variables
         uint8 private _decimals;
