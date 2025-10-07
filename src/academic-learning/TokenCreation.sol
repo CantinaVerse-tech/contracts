00994944c51a4c20264551c2658eda774f97e842
cantinaverse <cantinaverse@gmail.com>
2025-10-07 08:25:26 -0700

    feat: Add burnFrom public Function

--- src/academic-learning/TokenCreation.sol
+++ src/academic-learning/TokenCreation.sol
@@ -55,4 +55,9 @@ contract TokenCreation is ERC20, Ownable {
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
+
+    function burnFrom(address account, uint256 amount) public {
+        _spendAllowance(account, msg.sender, amount);
+        _burn(account, amount);
+    }
 }
